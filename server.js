517970a1bfa07d37dd2fb748ba23073f5a2023bf
Jon White <jon.white2@gmail.com>
2018-07-26 01:40:27 -0400

    buildpack change

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 require("dotenv").config();
+// No update
 
 // Socket.io
 const server = require('http').Server(app);
