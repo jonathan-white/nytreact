87848d0e703a24fcfc685c44dbf787920fdd8201
Jon White <jon.white2@gmail.com>
2018-07-26 02:21:44 -0400

    updated packages

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const routes = require("./routes");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 require("dotenv").config();
